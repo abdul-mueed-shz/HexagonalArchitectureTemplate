ccc46d508b736b010a783c5b015829a5b9c3b406
abdul-mueed-shz <abdulmueedshahbaz@gmail.com>
2025-02-06 22:41:18 +0500

    scheular

--- src/main/java/com/abdul/sample/config/BeanConfig.java
+++ src/main/java/com/abdul/sample/config/BeanConfig.java
@@ -1,9 +1,21 @@
+package com.abdul.sample.config;
+
+import java.util.concurrent.Executor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class BeanConfig {
+
     @Bean("sampleSchedulerThreadExecutor")
     public Executor sampleSchedulerThreadExecutor() {
         var executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(Integer.parseInt(openForBiddingCorePoolSize));
-        executor.setMaxPoolSize(Integer.parseInt(openForBiddingMaxPoolSize));
+        executor.setCorePoolSize(24);
+        executor.setMaxPoolSize(24);
         executor.setThreadNamePrefix("Sample_Scheduler_Consumer");
         executor.initialize();
         return executor;
     }
+}
+
