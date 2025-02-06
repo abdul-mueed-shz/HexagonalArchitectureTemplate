112e5aa351c573f94ee425d9168c9d0a18c65a03
abdul-mueed-shz <abdulmueedshahbaz@gmail.com>
2025-02-06 18:33:31 +0500

    schedular config

--- src/main/java/com/abdul/sample/adapter/in/schedular/sampleactionjob/SampleJobConsumer.java
+++ src/main/java/com/abdul/sample/adapter/in/schedular/sampleactionjob/SampleJobConsumer.java
@@ -1,23 +1,23 @@
-package com.olab.delegationservice.adapter.in.scheduler.closeorderperjob;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Component;
-
-@RequiredArgsConstructor
-public class CloseJobOrderConsumer implements Consumer {
-
-    private final CompleteSampleJobUseCase completeSampleJobUseCase;
-
-    /**
-     * update order completion jobs to complete order.
-     * @param id Long
-     */
-    @Override
-    @Async("sampleSchedulerThreadExecutor")
-    public synchronized void consume(Long id) {
-        completeSampleJobUseCase.completeJobOrder(id);
-    }
-}
-
-
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.scheduling.annotation.Async;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class SampleJobConsumer implements Consumer {
+//
+//    private final CompleteSampleJobUseCase completeSampleJobUseCase;
+//
+//    /**
+//     * update order completion jobs to complete order.
+//     * @param id Long
+//     */
+//    @Override
+//    @Async("sampleSchedulerThreadExecutor")
+//    public synchronized void consume(Long id) {
+//        completeSampleJobUseCase.completeJobOrder(id);
+//    }
+//}
+//
+//
--- src/main/java/com/abdul/sample/adapter/in/schedular/sampleactionjob/SampleJobProducer.java
+++ src/main/java/com/abdul/sample/adapter/in/schedular/sampleactionjob/SampleJobProducer.java
@@ -1,35 +1,36 @@
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-
-@RequiredArgsConstructor
-public class SampleJobProducer implements Producer {
-
-    private final SampleJobUseCase sampleJobUseCase;
-
-    private final Set<Long> processedIds = ConcurrentHashMap.newKeySet();
-
-    /**
-     * fetch jobs to close order.
-     *
-     * @return Long
-     */
-    @Override
-    public List<Long> produce() {
-        List<Long> unprocessedIds = sampleJobUseCase.fetchAllJobForToday(Timestamp.valueOf(LocalDateTime.now()),
-                Collections.singletonList(SampleEnums.PENDING.value()),
-                SampleEnums.COMPLETE.value());
-        List<Long> newIds = unprocessedIds.stream()
-                .filter(id -> !processedIds.contains(id))
-                .collect(Collectors.toList());
-        processedIds.addAll(newIds);
-
-        return newIds;
-    }
-}
+//import java.sql.Timestamp;
+//import java.time.LocalDateTime;
+//import java.util.Collections;
+//import java.util.List;
+//import java.util.Set;
+//import java.util.concurrent.ConcurrentHashMap;
+//import java.util.stream.Collectors;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class SampleJobProducer implements Producer {
+//
+//    private final SampleJobUseCase sampleJobUseCase;
+//
+//    private final Set<Long> processedIds = ConcurrentHashMap.newKeySet();
+//
+//    /**
+//     * fetch jobs to close order.
+//     *
+//     * @return Long
+//     */
+//    @Override
+//    public List<Long> produce() {
+//        List<Long> unprocessedIds = sampleJobUseCase.fetchAllJobForToday(Timestamp.valueOf(LocalDateTime.now()),
+//                Collections.singletonList(SampleEnums.PENDING.value()),
+//                SampleEnums.COMPLETE.value());
+//        List<Long> newIds = unprocessedIds.stream()
+//                .filter(id -> !processedIds.contains(id))
+//                .collect(Collectors.toList());
+//        processedIds.addAll(newIds);
+//
+//        return newIds;
+//    }
+//}
